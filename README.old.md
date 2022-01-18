ebf1d872f7b73495d91de76f6e7c9597ba73cbec
Callum Lees <hi@c-l.dev>
2022-01-18 23:48:31 +0000

    Initial React build with TailwindCSS

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# kraken-vpn-reactjs
+ A site for a VPN service provider called Kraken
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="">
+      <div className="flex justify-center items-center h-screen">
+        <h2 class="text-5xl sm:text-6xl text-left sm:text-center font-extrabold tracking-tight text-gray-900 mb-4">
+          <span className="block tracking-tighter">KrakenVPN</span>
+          <span className="uppercase tracking-widest font-normal block text-sm text-left text-gray-500">
+          Fast | Secure | Open-source
+        </span>
+        </h2>
+        
+      </div>
+    </div>
+  )
+}
+
+export default App;
